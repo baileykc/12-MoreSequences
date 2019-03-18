b509bc6b72c57ee572e21c07424bd5c4250460d4
baileykc <baileykc@rose-hulman.edu>
2019-03-18 13:56:57 -0400

    done

--- src/m4_accumulating_sequences.py
+++ src/m4_accumulating_sequences.py
@@ -397,7 +397,7 @@ def rectangles_from_circles(circles):
       :rtype: list of rg.Rectangles
     """
     # -------------------------------------------------------------------------
-    # Done: 10. Implement and test this function.
+    # DONE: 10. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ###########################################################################
